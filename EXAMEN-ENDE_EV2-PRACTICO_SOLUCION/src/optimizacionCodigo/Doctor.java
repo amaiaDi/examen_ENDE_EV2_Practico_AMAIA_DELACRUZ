bf505f4f8793af568c58e4ebfbb2e0b300d1031e
amaia.di <amaia.di@icjardin.com>
2023-02-12 17:53:17 +0100

    Validación inicial

---
+++ EXAMEN-ENDE_EV2-PRACTICO_SOLUCION/src/optimizacionCodigo/Doctor.java
@@ -0,0 +1,19 @@
+package  optimizacionCodigo ;
+
+import java.util.Date;
+
+public class Doctor {
+	private String nombre;
+	private String apellido;
+	private Date fechaDeNacimiento;
+	private int numeroColegiadoMedicina;
+
+	public void realizarDiagnostico() {
+	}
+
+	public void visitarPaciente() {
+	}
+
+	public void vertificarAlta() {
+	}
+}
---
+++ EXAMEN-ENDE_EV2-PRACTICO_SOLUCION/src/optimizacionCodigo/Enfermero.java
@@ -0,0 +1,19 @@
+package optimizacionCodigo;
+
+import java.util.Date;
+
+public class Enfermero {
+	private String nombre;
+	private String apellido;
+	private Date fechaDeNacimiento;
+	private int numeroColegiadoEnfermeria;
+
+	public void realizarCuras() {
+	}
+
+	public void visitarPaciente() {
+	}
+
+	public void contactarFamilias() {
+	}
+}
